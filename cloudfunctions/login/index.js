bfab5ba68547c345880fb783dd46d25acb8e5500
Zephyr <oncwnuPb9J_0j_bEJjXv8Pl85sjE@git.weixin.qq.com>
2020-12-21 21:58:29 +0800

    登录完善

--- cloudfunctions/login/index.js
+++ cloudfunctions/login/index.js
@@ -26,11 +26,7 @@ exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
 
   return {
-    event,
     openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
-    env: wxContext.ENV,
   }
 }
 
--- miniprogram/pages/user/user.js
+++ miniprogram/pages/user/user.js
@@ -6,75 +6,74 @@ Page({
    * 页面的初始数据
    */
   data: {
-    isLogin:false
+    isLogin: false
   },
-  myclass(e){
-   
-  },
-  homework(e){
+  myclass(e) {
 
   },
-  download(e){
+  homework(e) {
 
-  } ,  
-  change:function(){
+  },
+  download(e) {
+
+  },
+  change: function () {
 
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    let that = this
-  
     if (app.globalData.userInfo) {
-      console.log("1")
       this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true,
-        
+        isLogin: true
       })
-      console.log(this.data.userInfo.avatarUrl)
-      this.setData({
-        user_ima:this.data.userInfo.avatarUrl
-      })
-      
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-        console.log("2")
-      }
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          that.setData({
-            userInfo: res.userInfo,
-            isLogin: false
-          })
-          
+      wx.getSetting({
+        success: (res) => {
+          if (res.authSetting['scope.userInfo']) {
+            wx.getUserInfo({
+              lang: 'zh_CN',
+              success: res => {
+                let  userInfo = res.userInfo
+                this.add(userInfo)
+                this.setData({
+                  isLogin: true
+                })
+              }
+            })
+          }
         }
       })
-      console.log(that.data.isLogin)
-      console.log("3")
-      console.log(app.globalData.userInfo)
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    console.log("11111")
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      isLogin: true
+  getUserInfo: function (e) {
+    if (e.detail.userInfo) {
+      this.add(e.detail.userInfo)
+      this.setData({
+        isLogin: true
+      })
+    } else {
+
+      wx.showToast({
+        title: '需要授权哦',
+        icon: 'none'
+      })
+    }
+  },
+  add: function (e) {
+    const db = wx.cloud.database()
+    const user = db.collection('User')
+    user.add({
+      data: {
+        neckname: e.nickName,
+        sco: '',
+        phone: '',
+        class: ''
+      }
+
     })
   },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
